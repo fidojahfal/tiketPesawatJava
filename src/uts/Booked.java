aba68cfe7691877294ac3fbcdc8106e3b5737d5a
Admin <Admin@DESKTOP-D33KIOF>
2022-05-21 13:03:08 +0700

    Files Added: Booked.java ElementBooked.java ListBooked.java Minor changes to ListBooked.java

---
+++ src/uts/Booked.java
@@ -0,0 +1,16 @@
+package uts;
+
+public class Booked {
+    String id;
+    String nama;
+    String jadwal;
+    String kursi;
+    String kelas;
+    Booked(String id, String nama, String jadwal, String kursi, String kelas) {
+        this.id = id;
+        this.nama = nama;
+        this.jadwal = jadwal;
+        this.kursi = kursi;
+        this.kelas = kelas;
+    }
+}
---
+++ src/uts/ElementBooked.java
@@ -0,0 +1,12 @@
+package uts;
+
+public class ElementBooked {
+    Booked data;
+    ElementBooked next;
+    ElementBooked prev;
+    public ElementBooked(String id, String nama, String jadwal, String kursi, String kelas) {
+        data = new Booked(id, nama, jadwal, kursi, kelas);
+        next = null;
+        prev = null;
+    }
+}
